52a6b2920248eb2fa25664265ead3ded5a3616b5
Facultad Contaduría y Administración <facultadcontaduriayadministracion@MacBook-13.local>
2026-08-25 14:51:30 -0600

    Commit con la actividad 4 realizada en clase

---
+++ 25-ago-26/Act4.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let nombre = "Tona"
+
+let favoriteQuote = "'I'm a Swiftian'"
+
+print(favoriteQuote)
+
+print("Hola, io soi "+nombre+" y mi quote favorita es: "+favoriteQuote)
+
+let emptyString = ""
+
+if emptyString.isEmpty {
+    print("Aquí no hay naida")
+} else{
+    print("No está tan vacío como pense")
+}
+
