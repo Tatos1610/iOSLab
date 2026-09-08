87909a55ca4c244aa3f47f63daf969951aa3dc7e
Alexcast0 <alexnuno218@gmail.com>
2026-09-08 15:08:12 -0600

    Commit con las actividades 1,2 y 3 hechas en clase

---
+++ 08-09-26/Actividad3/Actividad3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad3
+//
+//  Created by Facultad Contaduría y Administración on 08/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
