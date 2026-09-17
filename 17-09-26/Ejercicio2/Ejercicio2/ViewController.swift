233cec316e9487abf9f6a68258d2bbe9affe456f
Alexcast0 <tonatiuhperez1999@gmail.com>
2026-09-17 15:04:43 -0600

    Commit con ejercicio 1 del 17 sept

---
+++ 17-09-26/Ejercicio2/Ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by Facultad Contaduría y Administración on 17/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 17-09-26/ejercicio1.playground/Contents.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+let userInputAge = "34e"
+let userAge: Int? = Int(userInputAge)
